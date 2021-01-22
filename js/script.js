6b2ae00d8291f63e826410bfcc640e99eec53e58
Nader Frigui <nadfri@gmail.com>
2021-01-22 18:51:24 +0100

    add Service Worker, manifest and icons

---
+++ js/script.js
@@ -0,0 +1,32 @@
+window.onbeforeinstallprompt = (event) => {
+    event.preventDefault(); // annuler la banniere par defaut
+    installBtn.classList.add("slide"); //affiche la banniere perso
+    setTimeout(() => installBtn.classList.remove("slide"), 8000);
+    setTimeout(() => installBtn.style.display = "none", 9000);
+
+    installBtn.onclick = () => {
+        installBtn.classList.remove("slide"); //faire disparaitre le bouton
+        setTimeout(() => installBtn.style.display = "none", 500);
+        event.prompt(); //permettre l'installation
+    };
+};
+
+
+//*************Service Worker ******************/
+//Register service worker to control making site work offline
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('js/sw.js')
+            .then(registration => {
+                console.log(
+                    `Service Worker enregistré ! Ressource: ${registration.scope}`
+                );
+            })
+            .catch(err => {
+                console.log(
+                    `Echec de l'enregistrement du Service Worker: ${err}`
+                );
+            });
+    });
+}
