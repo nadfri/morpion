16a3548e2a31ab9133c1e82f823d487012f4ee19
Nader Frigui <nadfri@gmail.com>
2021-01-24 16:09:26 +0100

    Refacto solo.js

--- js/script_solo.js
+++ js/script_solo.js
@@ -1,10 +1,11 @@
 "use strict";
-let player1 = true;
 let player1Begin = true;
-let gameOver = false;
-let round = 0;
-let tabPlayer1 = [];
-let tabPlayer2 = [];
+let player1      = true;
+let gameOver     = false;
+let tabPlayer1   = [];
+let tabPlayer2   = [];
+let round        = 0;
+
 const boxes = document.querySelectorAll(".box");
 const tabOfWins = [
   [8, 1, 6],
@@ -23,10 +24,10 @@ const tabOfWins = [
 |4|9|2| => 15
 */
 
-//Adding Events Click on all box
+/****************Adding Events Click on all box****************/
 for (let box of boxes) box.onclick = () => play(box);
 
-//Function of play
+/****************Player and IA play****************/
 function play(box) {
   if (box.textContent == "") {
     box.style.color = "blue";
@@ -34,74 +35,21 @@ function play(box) {
     box.textContent = "x";
     tabPlayer1[box.dataset.value] = parseInt(box.dataset.value);
     checkWin("Player ONE", tabPlayer1);
+
+    /*IA Plays*/
     skyNetplay();
-  } else {
-    //box not empty
+  } 
+
+  else 
+  {
+    /*box not empty*/
     box.style.backgroundColor = "red";
     setTimeout(() => (box.style.backgroundColor = "#11698e"), 500);
   }
 }
 
-/*Win Checking*/
-function checkWin(player, tabPlayer) {
-  round++;
 
-  for (let win of tabOfWins) {
-    let victory = 0;
-
-    for (let value of win) {
-      victory += tabPlayer[value];
-    }
-
-    if (victory == 15) {
-      gameOver = true; //game is over
-      /*Score*/
-      if (player == "Player ONE") scoreOne.textContent++;
-      else scoreTwo.textContent++;
-
-      newGame.innerHTML = `<div>
-                            <p>${player} Wins!!!</p>
-                            <p>New Game? Click Here...</p>
-                            </div>`;
-
-      newGame.style.display = "flex";
-      newGame.onclick = reset;
-    }
-  }
-
-  /*Case of Equality, Max 9 rounds*/
-  if (round == 9 && gameOver == false) {
-    gameOver = true;
-    newGame.innerHTML = `<div>
-                         <p>Equality!!!<p>
-                         <p>New Game? Click Here...</p>
-                        </div>`;
-
-    newGame.style.display = "flex";
-    newGame.onclick = reset;
-  }
-}
-
-/*Reset the Game*/
-function reset() {
-  newGame.style.display = "none";
-  tabPlayer1 = [];
-  tabPlayer2 = [];
-  round = 0;
-  gameOver = false;
-
-  for (let box of boxes) {
-    box.textContent = "";
-    box.style.backgroundColor = "#11698e";
-    box.style.color = "white";
-  }
-  player1Begin = !player1Begin;
-  if (!player1Begin) skyNetplay();
-}
-/*Reset Button*/
-resetBTN.onclick = reset;
-
-/*IA of SkyNet*/
+/****************IA of SkyNet****************/
 function choiceIA(tabPlayer)
 {
     for (let win of tabOfWins)
@@ -112,8 +60,21 @@ function choiceIA(tabPlayer)
             return filter[0];
     }
 }
+/*Random IA Choices*/
+function randomChoice()
+{
+    /*Array.from transform querySelectorAll to a true array*/
+    let empty = Array.from(boxes).filter(
+        (box) => box.textContent == "");
 
-/*Choice of IA*/
+    let max    = empty.length - 1;
+    let random = Math.floor(Math.random() * (max + 1));
+    empty[random].textContent           = "o";
+    empty[random].style.backgroundColor = "#466f6f";
+    tabPlayer2[empty[random].dataset.value] = parseInt(empty[random].dataset.value);
+}
+
+/*Specific Choices of IA*/
 function skyNetplay() {
   if (gameOver == false) 
   {
@@ -122,41 +83,92 @@ function skyNetplay() {
       /*If IA first Player, plays center box*/  
       if (box5.textContent == "") 
       {
-        box5.textContent = "o";
         box5.style.backgroundColor = "#466f6f";
-        tabPlayer2[5] = 5;
+        box5.textContent = "o";
+        tabPlayer2[5]    = 5;
       } 
 
       else
       {
         /*If IA fisrt Player plays attack, else plays defense*/
         let tabPlayer = player1Begin? tabPlayer1 : tabPlayer2;
-        let choice  = choiceIA(tabPlayer);
+        let choice    = choiceIA(tabPlayer);
 
         if(choice)
         {
             tabPlayer2[choice] = choice;
             let id = document.getElementById("box"+choice);
-            id.textContent = "o";
+            id.textContent           = "o";
             id.style.backgroundColor = "#466f6f";
         }
 
-        else
-        {
-            /*If no risk, Ia plays random*/
-            let emptyBoxes = Array.from(boxes).filter(
-            (box) => box.textContent == "");
-
-            let max = emptyBoxes.length - 1;
-            let randomBox = Math.floor(Math.random() * (max + 1));
-            emptyBoxes[randomBox].textContent = "o";
-            emptyBoxes[randomBox].style.backgroundColor = "#466f6f";
-            tabPlayer2[emptyBoxes[randomBox].dataset.value] = parseInt(emptyBoxes[randomBox].dataset.value
-            );
-        }
+        else randomChoice();
 
       }
       checkWin("SkyNet", tabPlayer2);
     }, 100);
   }
 }
+
+/****************Win Checking****************/
+function checkWin(player, tabPlayer) {
+    round++;
+  
+    for (let win of tabOfWins) {
+      let victory = 0;
+  
+      for (let value of win) {
+        victory += tabPlayer[value];
+      }
+  
+      if (victory == 15) 
+      {
+        gameOver = true; //game is over
+        /*Score update*/
+        if (player == "Player ONE") scoreOne.textContent++;
+        else scoreTwo.textContent++;
+  
+        newGame.innerHTML = `<div>
+                              <p>${player} Wins!!!</p>
+                              <p>New Game? Click Here...</p>
+                              </div>`;
+  
+        newGame.style.display = "flex";
+        newGame.onclick       = reset;
+      }
+    }
+  
+    /*Case of Equality, Max 9 rounds*/
+    if (round == 9 && gameOver == false) 
+    {
+      gameOver = true;
+      newGame.innerHTML = `<div>
+                           <p>Equality!!!<p>
+                           <p>New Game? Click Here...</p>
+                          </div>`;
+  
+      newGame.style.display = "flex";
+      newGame.onclick       = reset;
+    }
+  }
+  
+  /****************Reset the Game****************/
+  function reset() {
+    newGame.style.display = "none";
+    tabPlayer1 = [];
+    tabPlayer2 = [];
+    round      = 0;
+    gameOver   = false;
+  
+    for (let box of boxes) 
+    {
+      box.style.backgroundColor = "#11698e";
+      box.textContent = "";
+      box.style.color = "white";
+    }
+    player1Begin = !player1Begin;
+    if (!player1Begin) skyNetplay();
+  }
+
+  /*Reset Button*/
+  resetBTN.onclick = reset;
