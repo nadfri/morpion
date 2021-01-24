072a7ea8cad4ed680b4df48d65d9aca277ea2908
Nader Frigui <nadfri@gmail.com>
2021-01-24 14:24:00 +0100

    IA Defense OK

--- js/script_solo.js
+++ js/script_solo.js
@@ -7,128 +7,197 @@ let tabPlayer1 = [];
 let tabPlayer2 = [];
 const boxes = document.querySelectorAll(".box");
 const tabOfWins = [
-    ["1", "2", "3"],
-    ["4", "5", "6"],
-    ["7", "8", "9"],
-    ["1", "5", "9"],
-    ["7", "5", "3"],
-    ["1", "4", "7"],
-    ["2", "5", "8"],
-    ["3", "6", "9"],
+  [8, 1, 6],
+  [3, 5, 7],
+  [4, 9, 2],
+  [8, 3, 4],
+  [1, 5, 9],
+  [6, 7, 2],
+  [8, 5, 2],
+  [6, 5, 4],
 ];
 
+/*Magic Square
+|8|1|6| => 15
+|3|5|7| => 15
+|4|9|2| => 15
+*/
 
 //Adding Events Click on all box
-for (let box of boxes)
-    box.onclick = () => play(box);
+for (let box of boxes) box.onclick = () => play(box);
 
 //Function of play
 function play(box) {
-    if (box.textContent == "") {
-
-        box.style.color = "blue";
-        box.style.backgroundColor = "teal";
-        box.textContent = "x";
-        tabPlayer1.push(box.dataset.value);
-        checkWin("Player ONE", tabPlayer1);
-        skyNetplay();
-
-    } else {
-        //box not empty
-        box.style.backgroundColor = "red";
-        setTimeout(() => (box.style.backgroundColor = "#11698e"), 500);
-    }
+  if (box.textContent == "") {
+    box.style.color = "blue";
+    box.style.backgroundColor = "teal";
+    box.textContent = "x";
+    tabPlayer1[box.dataset.value] = parseInt(box.dataset.value);
+    checkWin("Player ONE", tabPlayer1);
+    skyNetplay();
+  } else {
+    //box not empty
+    box.style.backgroundColor = "red";
+    setTimeout(() => (box.style.backgroundColor = "#11698e"), 500);
+  }
 }
 
 /*Win Checking*/
 function checkWin(player, tabPlayer) {
-    round++;
+  round++;
 
-    for (let win of tabOfWins) {
-        let count = 0;
-        let result = [];
+  for (let win of tabOfWins) {
+    let victory = 0;
 
-        for (let value of tabPlayer) {
-            if (win.includes(value)) {
-                count++;//if 3 values found, player wins
-                result.push(value);
-            }
-        }
-
-        if (count == 3) {
-            for (let box of boxes)
-                if (result.includes(box.dataset.value))
-                    box.style.backgroundColor = "green";
-
-            gameOver = true; //game is over
-
-            /*Score*/
-            if (player == "Player ONE") scoreOne.textContent++;
-            else scoreTwo.textContent++;
-
-            newGame.innerHTML = `<div>
-                                    <p>${player} Wins!!!</p>
-                                    <p>New Game? Click Here...</p>
-                                 </div>`;
-
-            newGame.style.display = "flex";
-            newGame.onclick = reset;
-        }
+    for (let value of win) {
+      victory += tabPlayer[value];
     }
 
-    /*Case of Equality, Max 9 rounds*/
-    if (round == 9 && gameOver == false) {
-        newGame.innerHTML = `<div>
-                                <p>Equality!!!<p>
-                                <p>New Game? Click Here...</p>
-                             </div>`;
-        newGame.style.display = "flex";
-        newGame.onclick = reset;
+    if (victory == 15) {
+      gameOver = true; //game is over
+      /*Score*/
+      if (player == "Player ONE") scoreOne.textContent++;
+      else scoreTwo.textContent++;
+
+      newGame.innerHTML = `<div>
+                            <p>${player} Wins!!!</p>
+                            <p>New Game? Click Here...</p>
+                            </div>`;
+
+      newGame.style.display = "flex";
+      newGame.onclick = reset;
     }
+  }
+
+  /*Case of Equality, Max 9 rounds*/
+  if (round == 9 && gameOver == false) {
+    newGame.innerHTML = `<div>
+                         <p>Equality!!!<p>
+                         <p>New Game? Click Here...</p>
+                        </div>`;
+
+    newGame.style.display = "flex";
+    newGame.onclick = reset;
+  }
 }
 
 /*Reset the Game*/
 function reset() {
-    newGame.style.display = "none";
-    tabPlayer1 = [];
-    tabPlayer2 = [];
-    round = 0;
-    gameOver = false;
+  newGame.style.display = "none";
+  tabPlayer1 = [];
+  tabPlayer2 = [];
+  round = 0;
+  gameOver = false;
 
-    for (let box of boxes) {
-        box.textContent = "";
-        box.style.backgroundColor = "#11698e";
-        box.style.color = "white";
-    }
-    player1Begin = !player1Begin;
-    if (!player1Begin) skyNetplay();
+  for (let box of boxes) {
+    box.textContent = "";
+    box.style.backgroundColor = "#11698e";
+    box.style.color = "white";
+  }
+  player1Begin = !player1Begin;
+  if (!player1Begin) skyNetplay();
 }
 /*Reset Button*/
 resetBTN.onclick = reset;
 
 /*IA of SkyNet*/
 function skyNetplay() {
+  if (gameOver == false) 
+  {
+    setTimeout(() => 
+    {
+        /*Test if 2 cases checked*/
+        let filter = [];
+        for (let win of tabOfWins)
+        {
+             filter = win.filter(el=> !tabPlayer1.includes(el));
+             if(filter.length == 1 && document.getElementById("box"+filter[0]).textContent =="")
+             {
+                console.log(filter)
+                break;
+             }
+        }
 
-    if (gameOver == false) {
-        setTimeout(() => {
-            if (boxes[4].textContent == "") {
-                boxes[4].textContent = "o";
-                boxes[4].style.backgroundColor = "#466f6f";
-                tabPlayer2.push("5");
-            }
-            else {
-                let emptyBoxes = Array.from(boxes).filter(box => box.textContent == "");
-                let max = emptyBoxes.length - 1;
-                let randomBox = Math.floor(Math.random() * (max + 1));
-                emptyBoxes[randomBox].textContent = "o";
-                emptyBoxes[randomBox].style.backgroundColor = "#466f6f";
-                tabPlayer2.push(emptyBoxes[randomBox].dataset.value);
-            }
-
+        /*IA play defense*/
+        if(filter.length == 1)
+        {
+            tabPlayer2[filter[0]] = parseInt(filter[0]);
+            let id = document.getElementById("box"+filter[0]);
+            id.textContent = "o";
+            id.style.backgroundColor = "#466f6f";
             checkWin("SkyNet", tabPlayer2);
+        }
 
-        }, 100);
-    }
+        else
+        {
+            let emptyBoxes = Array.from(boxes).filter(
+            (box) => box.textContent == "");
+
+            let max = emptyBoxes.length - 1;
+            let randomBox = Math.floor(Math.random() * (max + 1));
+            emptyBoxes[randomBox].textContent = "o";
+            emptyBoxes[randomBox].style.backgroundColor = "#466f6f";
+            tabPlayer2[emptyBoxes[randomBox].dataset.value] = parseInt(emptyBoxes[randomBox].dataset.value
+            );
+            checkWin("SkyNet", tabPlayer2);
+        }
+
+
+
+
+    //   if (box5.textContent == "") 
+    //   {
+    //     box5.textContent = "o";
+    //     box5.style.backgroundColor = "#466f6f";
+    //     tabPlayer2[5] = 5;
+    //   } 
+
+    //   else 
+    //   {
+    //     let played = [...tabPlayer1, ...tabPlayer2];
+
+    //     for (let win of tabOfWins) 
+    //     {
+
+    //         let count = [];
+
+    //         for(let element of win)
+    //         {
+    //             if(!played.includes(element) && element)
+    //                 count.push(element);
+    //         }
+
+    //         console.log(count)     
+    //         if (count.length == 1) 
+    //         {
+    //             console.log("IA")
+    //             tabPlayer2[count[0]] = parseInt(count[0]);
+    //             let id = document.getElementById("box"+count[0]);
+    //             id.textContent = "o";
+    //             id.style.backgroundColor = "#466f6f";
+    //             checkWin("SkyNet", tabPlayer2);
+    //             return;
+    //         } 
+
+    //         else
+    //         {
+    //             let emptyBoxes = Array.from(boxes).filter(
+    //                 (box) => box.textContent == ""
+    //             );
+
+    //             let max = emptyBoxes.length - 1;
+    //             let randomBox = Math.floor(Math.random() * (max + 1));
+    //             emptyBoxes[randomBox].textContent = "o";
+    //             emptyBoxes[randomBox].style.backgroundColor = "#466f6f";
+    //             tabPlayer2[emptyBoxes[randomBox].dataset.value] = parseInt(emptyBoxes[randomBox].dataset.value
+    //             );
+    //             checkWin("SkyNet", tabPlayer2);
+    //             return;
+    //         }  
+    //     }
+    //   }
+
+      //checkWin("SkyNet", tabPlayer2);
+    }, 100);
+  }
 }
-
-
--- sw.js
+++ sw.js
@@ -1,30 +1,30 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-console.log(`WorkBox of Morpion loaded🎉`);
+// console.log(`WorkBox of Morpion loaded🎉`);
 
-const { registerRoute } = workbox.routing;
-const { StaleWhileRevalidate } = workbox.strategies;
-const { CacheFirst } = workbox.strategies;
-const { ExpirationPlugin } = workbox.expiration;
+// const { registerRoute } = workbox.routing;
+// const { StaleWhileRevalidate } = workbox.strategies;
+// const { CacheFirst } = workbox.strategies;
+// const { ExpirationPlugin } = workbox.expiration;
 
-registerRoute(
-    ({ request }) => request.destination === 'document' ||
-        request.destination === 'script',
-    new StaleWhileRevalidate({ cacheName: 'files-cache' })
-);
+// registerRoute(
+//     ({ request }) => request.destination === 'document' ||
+//         request.destination === 'script',
+//     new StaleWhileRevalidate({ cacheName: 'files-cache' })
+// );
 
-registerRoute(
-    ({ request }) => request.destination === 'style',
-    new StaleWhileRevalidate({ cacheName: 'css-cache' })
-);
+// registerRoute(
+//     ({ request }) => request.destination === 'style',
+//     new StaleWhileRevalidate({ cacheName: 'css-cache' })
+// );
 
-registerRoute(
-    ({ url }) => url.origin === 'https://fonts.googleapis.com' ||
-        url.origin === 'https://fonts.gstatic.com',
-    new StaleWhileRevalidate({
-        cacheName: 'google-fonts',
-        plugins: [
-            new ExpirationPlugin({ maxEntries: 20 }),
-        ],
-    }),
-);
+// registerRoute(
+//     ({ url }) => url.origin === 'https://fonts.googleapis.com' ||
+//         url.origin === 'https://fonts.gstatic.com',
+//     new StaleWhileRevalidate({
+//         cacheName: 'google-fonts',
+//         plugins: [
+//             new ExpirationPlugin({ maxEntries: 20 }),
+//         ],
+//     }),
+// );
